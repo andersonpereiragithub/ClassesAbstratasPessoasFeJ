a34e8fa1e39f77b603b20499d73ea94129e6323c
Anderson Pereira <anderson.pereira@viannasempre.com.br>
2021-05-21 08:01:17 -0300

    Correções com o Gabarito do professor

--- ClassesAbstratasExercicio/Program.cs
+++ ClassesAbstratasExercicio/Program.cs
@@ -10,6 +10,7 @@ namespace ClassesAbstratasExercicio
         static void Main(string[] args)
         {
             List<Pessoa> lista = new List<Pessoa>();
+
             Console.Write("Insira o número de contribuintes: ");
             int n = int.Parse(Console.ReadLine());
 
@@ -19,43 +20,34 @@ namespace ClassesAbstratasExercicio
                 Console.Write("Pessoa física ou jurídica (i/c)? ");
                 char ch = char.Parse(Console.ReadLine().ToLower());
 
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Renda Anual: ");
+                double renda = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
                 if (ch == 'i')
                 {
-                    Console.Write("Nome: ");
-                    string nome = Console.ReadLine();
-
-                    Console.Write("Renda Anual: ");
-                    double renda = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
                     Console.Write("Despesas com saúde: ");
                     double gastosSaude = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
                     lista.Add(new PessoaFisica(nome, renda, gastosSaude));
                 }
-                else if (ch == 'c')
+                else
                 {
-                    Console.Write("Nome: ");
-                    string nome = Console.ReadLine();
-
-                    Console.Write("Renda Anual: ");
-                    double renda = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
                     Console.Write("Número de Empregrados: ");
                     int numEmpregados = int.Parse(Console.ReadLine());
                     lista.Add(new PessoaJuridica(nome, renda, numEmpregados));
                 }
             }
+            double soma = 0;
             Console.WriteLine("\nImpostos Pagos: ");
             foreach (Pessoa p in lista)
             {
                 Console.WriteLine($"{p.Nome}: $ {p.CalculaImposto().ToString("F2", CultureInfo.InvariantCulture)}");
+                soma += p.CalculaImposto();
             }
 
             Console.Write("\nIMPOSTOS TOTAIS: ");
-            double soma = 0;
-            foreach (Pessoa p in lista)
-            {
-                soma += p.CalculaImposto();
-            }
             Console.Write(soma.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
